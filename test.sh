f41a9e90b299c7cfe0c583fba98ce115861c91e5
Adrian Mouat <adrian.mouat@gmail.com>
2017-03-28 15:26:16 +0100

    Test again.

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ ID=$! # ID of webserver process, so we can kill it
 tests_passed=true
 expected="Hello From Golang"
 output=$(curl -s localhost:8080)
-if [[ "$expected" != "$output" ]]; then
+if [ "$expected" != "$output" ]; then
   echo "Test Failure"
   echo "$expected != $output"
   tests_passed=false
@@ -15,7 +15,7 @@ fi
 
 kill $ID
 
-if [[ "$tests_passed" == "true" ]]; then
+if [ "$tests_passed" == "true" ]; then
   echo "Passed Tests"
 else 
   echo "Failed Tests"
